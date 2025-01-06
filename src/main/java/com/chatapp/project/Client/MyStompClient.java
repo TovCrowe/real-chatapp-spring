97f21d0b9d915cc9d6148f023f94500b06eb4281
TovCrowe <manuelxdvic@gmail.com>
2025-01-06 10:35:10 -0600

    MyStompClient Fimisehd

--- src/main/java/com/chatapp/project/Client/MyStompClient.java
+++ src/main/java/com/chatapp/project/Client/MyStompClient.java
@@ -2,8 +2,11 @@ package com.chatapp.project.Client;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 import org.springframework.web.socket.sockjs.SockJsService;
@@ -12,11 +15,13 @@ import org.springframework.web.socket.sockjs.client.Transport;
 import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
 public class MyStompClient {
-    private StompSession stompSession;
+    private StompSession session;
     private String username;
 
-    public MyStompClient(String username){
+
+    public MyStompClient(String username) throws ExecutionException, InterruptedException {
         this.username = username;
+        
 
         //make the transport to comunicate properly with a websocket server
         List<Transport> transport = new ArrayList<>();
@@ -25,7 +30,11 @@ public class MyStompClient {
         SockJsClient sockJsClient = new SockJsClient(transport);
         //sockjsclient by itself it doesnt support the Stomp protocol so we create a websocketstompclient to hanle stomp protocol
         WebSocketStompClient stompClient = new WebSocketStompClient(sockJsClient);
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+        StompSessionHandler sessionHandler = new MyStompSessionHandler(username);
+        String url = "ws://localhost:8080/ws";
 
+        session = stompClient.connectAsync(url, sessionHandler).get();
         
     }
 
--- src/main/java/com/chatapp/project/Client/MyStompSessionHandler.java
+++ src/main/java/com/chatapp/project/Client/MyStompSessionHandler.java
@@ -1,5 +1,7 @@
 package com.chatapp.project.Client;
 
+import java.lang.reflect.Type;
+
 import org.springframework.messaging.simp.stomp.StompFrameHandler;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
@@ -8,28 +10,49 @@ import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 import com.chatapp.project.Models.Message;
 
 public class MyStompSessionHandler extends StompSessionHandlerAdapter {
+    private String username;
+
+    public MyStompSessionHandler(String username) {
+        this.username = username;
+    }
     @Override
     public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
-        session.subscribe("/topic/message", new StompFrameHandler() {
-            @Override
-            public Type getPayloadType(StompHeaders headers){
-                return Message.class;
-            }
-            @Override
-            public void handleFrame(StompHeaders headers, Object payload){
-                try {
-                    if(payload instanceof Message){
-                        Message message = (Message) payload;
-                        System.out.println("Recive message from user :" + message.getUser() + " Message : " + message.getMessage() );
-                    } else {
-                        System.out.println("Recive Unexpected payload type :" + payload.getClass());
-                    }
-                } catch (Exception e) {
-                    // TODO: handle exception
-                    e.printStackTrace();
+        System.out.println("Attempting subscription...");
+
+        try {
+            StompSession.Subscription subscription = session.subscribe("/topic/message", new StompFrameHandler() {
+                @Override
+                public Type getPayloadType(StompHeaders headers) {
+                    System.out.println("Getting payload type for headers: " + headers);
+                    return Message.class;
                 }
-            }
-        });
+
+                @Override
+                public void handleFrame(StompHeaders headers, Object payload) {
+                    System.out.println("Frame received - Headers: " + headers);
+                    try {
+                        if (payload instanceof Message) {
+                            Message message = (Message) payload;
+                            System.out.println("Message received: " + message.getMessage());
+                        } else {
+                            System.out.println("Unexpected payload: " + payload);
+                        }
+                    } catch (Exception e) {
+                        System.err.println("Error handling frame: " + e.getMessage());
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            System.out.println("Subscription successful: " + subscription.getSubscriptionId());
+        } catch (Exception e) {
+            System.err.println("Subscription failed: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void handleTransportError(StompSession session, Throwable exception) {
+         exception.printStackTrace();
     }
 
 
